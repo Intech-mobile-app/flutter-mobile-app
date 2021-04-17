bbdb2321c2d0e187d593d4da89f1f90434772831
Nithish <nithishchandrasekar555@gmail.com>
2021-04-17 19:17:27 +0530

    profile screen updated

--- fl_mobile_intech_frontend/lib/Components/ProfileScreen/image.dart
+++ fl_mobile_intech_frontend/lib/Components/ProfileScreen/image.dart
@@ -4,8 +4,10 @@ Widget image() {
   return Container(
       height: 150.0,
       width: 150.0,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
-      child: Image(
-          image: NetworkImage(
+      child: CircleAvatar(
+          radius: 10.0,
+          backgroundImage: NetworkImage(
               'https://uploads-ssl.webflow.com/6030077fdbd53858ff7c4765/603c1ac00b9e8a080528b4ae_SalonBrillareGenericProfileAvi.jpg')));
 }
+
+
--- fl_mobile_intech_frontend/lib/Screens/Profile/profile_tab.dart
+++ fl_mobile_intech_frontend/lib/Screens/Profile/profile_tab.dart
@@ -4,10 +4,11 @@ class ProfilePage extends StatelessWidget {
   final TextEditingController _namecontroller = TextEditingController();
   final TextEditingController _emailcontroller = TextEditingController();
   final TextEditingController _societycontroller = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return SingleChildScrollView(
+      controller: _scrollController,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -18,28 +19,34 @@ class ProfilePage extends StatelessWidget {
               margin: const EdgeInsets.only(left: 15.0),
               child: Text(
                 'Please give the following details',
-                style: TextStyle(fontSize: 17.0),
+                style: TextStyle(fontSize: 15.0,fontWeight: FontWeight.w500),
               )),
           Padding(
             padding:
-                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                const EdgeInsets.only(top: 15.0,bottom: 5.0,left: 15.0,right: 15.0),
             child: textwidget(context, _namecontroller, 'Name'),
           ),
           Padding(
             padding:
-                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
             child: textwidget(context, _emailcontroller, 'Email'),
           ),
           Padding(
             padding:
-                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
             child: textwidget(context, _societycontroller, 'Society'),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Update the profile picture'),
-            ],
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 9.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Image.asset('Assets/Images/profile_upload.png',height: 22.0,width: 22.0,)),
+                Text('Update the profile picture'),
+              ],
+            ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
