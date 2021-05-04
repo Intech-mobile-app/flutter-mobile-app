d0e85c0dcf71ea7befa70f78bf495f8462f38efc
naveen <naveen.rm.551@gmail.com>
2021-05-04 21:52:02 +0530

    Issue #42 fixed

--- fl_mobile_intech_frontend/lib/Screens/Home/home_tab.dart
+++ fl_mobile_intech_frontend/lib/Screens/Home/home_tab.dart
@@ -16,11 +16,12 @@ class _HomePageState extends State<HomePage>
   var title;
   var message;
   var finalResponse;
+  dynamic dataLength = 0;
 
   Stream loadUrl() async* {
     var postResponse = await get(
       Uri.parse(
-        'https://llokality-intech-xald7lspga-el.a.run.app/api/v1.0/posts/0&6',
+        API.getAllPosts,
       ),
       headers: {'x-auth-token': authToken},
     );
@@ -31,13 +32,15 @@ class _HomePageState extends State<HomePage>
   getContents() async {
     var postResponse = await get(
       Uri.parse(
-        'https://llokality-intech-xald7lspga-el.a.run.app/api/v1.0/posts/0&6',
+        API.getAllPosts,
       ),
       headers: {'x-auth-token': authToken},
     );
     finalResponse = jsonDecode(postResponse.body);
     print('imageUrl : ${finalResponse['data'][0]['images']}');
     print('datalength : ${finalResponse['data'].length}');
+    dataLength = finalResponse['data'].length;
+    setState(() {});
   }
 
   getInstance() async {
@@ -58,6 +61,173 @@ class _HomePageState extends State<HomePage>
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
 
+    var dynamicContentsWidget = Column(
+      children: [
+        for (int i = 0; i < dataLength; i++)
+          StreamBuilder(
+            stream: loadUrl(),
+            builder: (context, snapshot) {
+              var fullName =
+                  finalResponse['data'][i]['user']['profile']['fullName'];
+              var houseNo =
+                  finalResponse['data'][i]['user']['profile']['houseNo'];
+              var title = finalResponse['data'][i]['title'];
+              var message = finalResponse['data'][i]['text'];
+              var postImages = finalResponse['data'][i]['images'];
+              var postId = finalResponse['data'][i]['_id'];
+              var comments = finalResponse['data'][i]['comments'];
+              bool isDeleted = finalResponse['data'][i]['isDeleted'];
+
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Colors.blue.shade100,
+                        radius: 25,
+                        child: FlutterLogo(
+                          size: 40,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '$fullName',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3,
+                          ),
+                          Text(
+                            '$houseNo',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w100,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.more_vert_outlined,
+                        color: Colors.black,
+                        size: 25,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    '$title',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '$message',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w100,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  postImages != null
+                      ? SizedBox(
+                          height: 120,
+                          child: ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: postImages.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                width: 170,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(
+                                      15,
+                                    ),
+                                  ),
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      postImages[index],
+                                    ),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              );
+                            },
+                            separatorBuilder:
+                                (BuildContext context, int index) {
+                              return SizedBox(
+                                width: 10,
+                              );
+                            },
+                          ),
+                        )
+                      : Container(),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    children: [
+                      Spacer(),
+                      Text(
+                        '${comments.length} Comments',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w100,
+                          letterSpacing: 0.5,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        Icons.question_answer_outlined,
+                        size: 22,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Divider(
+                    height: 1,
+                    color: Colors.black54,
+                  ),
+                ],
+              );
+            },
+          ),
+      ],
+    );
+
     return Scaffold(
       body: Container(
         height: height,
@@ -68,164 +238,7 @@ class _HomePageState extends State<HomePage>
               left: 20,
               right: 20,
             ),
-            child: Column(
-              children: [
-                for (int i = 0; i < finalResponse['data'].length; i++)
-                  StreamBuilder(
-                    stream: loadUrl(),
-                    builder: (context, snapshot) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                height: 20,
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.blue.shade100,
-                                radius: 25,
-                                child: FlutterLogo(
-                                  size: 40,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '${finalResponse['data'][i]['user']['profile']['fullName']}',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600,
-                                      letterSpacing: 0.5,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 3,
-                                  ),
-                                  Text(
-                                    '${finalResponse['data'][i]['user']['profile']['houseNo']}',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w100,
-                                      letterSpacing: 0.5,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Spacer(),
-                              Icon(
-                                Icons.more_vert_outlined,
-                                color: Colors.black,
-                                size: 25,
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            '${finalResponse['data'][i]['title']}',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 0.5,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '${finalResponse['data'][i]['text']}',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w100,
-                              letterSpacing: 0.5,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          finalResponse['data'][i]['images'].length != null
-                              ? SizedBox(
-                                  height: 120,
-                                  child: ListView.separated(
-                                    scrollDirection: Axis.horizontal,
-                                    itemCount: finalResponse['data'][i]
-                                            ['images']
-                                        .length,
-                                    itemBuilder: (context, index) {
-                                      return Container(
-                                        width: 170,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.all(
-                                            Radius.circular(
-                                              15,
-                                            ),
-                                          ),
-                                          image: DecorationImage(
-                                            image: NetworkImage(
-                                              finalResponse['data'][i]['images']
-                                                  [index],
-                                            ),
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                    separatorBuilder:
-                                        (BuildContext context, int index) {
-                                      return SizedBox(
-                                        width: 10,
-                                      );
-                                    },
-                                  ),
-                                )
-                              : Container(),
-                          SizedBox(
-                            height: 25,
-                          ),
-                          Row(
-                            children: [
-                              Spacer(),
-                              Text(
-                                '1 Comments',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w100,
-                                  letterSpacing: 0.5,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Icon(
-                                Icons.question_answer_outlined,
-                                size: 22,
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Divider(
-                            height: 1,
-                            color: Colors.black54,
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-              ],
-            ),
+            child: dynamicContentsWidget,
           ),
         ),
       ),
